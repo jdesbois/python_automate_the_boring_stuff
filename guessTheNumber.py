4bb81273319eabe5cbd066371f69249e767f8ca2
John Desbois <jdesbois@me.com>
2020-05-27 12:28:14 +0100

    fixed print statements for spelling mistakes

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,9 +1,9 @@
 #This is a guess the number game.
 import random
 secretNumber = random.randint(1,20)
-print('I am thining of a number netween 1 and 10.')
+print('I am thinking of a number between 1 and 20.')
 
-#As the player to guess 6 times
+#Ask the player to guess 6 times
 for guessesTaken in range(1,7):
     print('Take a guess.')
     guess = int(input())
@@ -16,7 +16,7 @@ for guessesTaken in range(1,7):
         break # This condition is the correct guess!
 
 if guess == secretNumber:
-    print('Good job! You guesse my number in ' + str(guessesTaken) + ' guesses!')
+    print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
 else:
     print('Nope. The number I was thinking of was ' + str(secretNumber))
 
