c2796bd3b3b1383b087a7285e9efa6bb0e2d5b2b
John Desbois <jdesbois@me.com>
2020-05-27 14:07:07 +0100

    added the first two lists files

---
+++ myPets.py
@@ -0,0 +1,8 @@
+# Searches list of pets to see entered pet is present
+myPets = ['Zophie', 'Pooka', 'Fat-tail']
+print('Enter a pet name:')
+name = input()
+if name not in myPets:
+    print('I do not have a pet named ' + name)
+else:
+    print(name + ' is my pet.')
